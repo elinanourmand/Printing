d5a94b62190c1dab1483297d984ae6943df77ac9
elinanourmand <54960818+elinanourmand@users.noreply.github.com>
2021-02-14 23:09:18 -0500

    Create Print.java

---
+++ Print.java
@@ -0,0 +1,9 @@
+/* This program will output "Hello World"
+ */
+
+public class Lab1 {
+	public static void main (String [] args) {
+		System.out.print("Hello World");
+
+	}// close main
+}// close class
